c9e38ba4d96e984f5501824a067e711f13deb022
laura <lauradondiego@gmail.com>
2019-09-05 15:43:21 -0400

    added heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// code away!
+require("dotenv").config();
 const server = require("./server");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+console.log(process.env.PORT);
+
 server.listen(port, () => console.log(`\n** API on port ${port} **\n`));
--- users/userRouter.js
+++ users/userRouter.js
@@ -142,15 +142,13 @@ function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
   console.log(req.body);
-  if (!req.body) {
-    res.status(400).json({
-      message: "missing user data"
-    });
-  } else if (!req.body.name) {
-    res.status(400).json({
-      message: "missing required name field"
-    });
+  if (Object.keys(req.body) < 1) {
+    return res.status(400).json({ message: "missing user data" });
   }
+  if (!req.body.name) {
+    return res.status(400).json({ message: "missing required name field" });
+  }
+  req.user = req.body;
   next();
 }
 
